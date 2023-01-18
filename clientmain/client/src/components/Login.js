3227f726aef76c8e739e4c308b69ff0993a749cd
shubhamShandilya <shubhflair@gmail.com>
2023-01-18 18:25:26 +0530

    Updated Signup page

--- clientmain/client/src/components/Login.js
+++ clientmain/client/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { Link, useHistory, useNavigate } from "react-router-dom";
--- clientmain/client/src/components/Signup.js
+++ clientmain/client/src/components/Signup.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import LoginNav from './LoginNav';
+import LoadingScreen from "react-loading-screen";
 /**import ' node_modules/bootstrap/dist/css/bootstrap.min.css'*/
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 export default function SignUp() {
+  const navigate = useNavigate();
+  const navigateTo = () => navigate("../home", { replace: true });
   const handleSubmit = async (e) => {
+    setLoadingState(true);
     e.preventDefault();
     const user = await axios.post('https://fliprslipr.onrender.com/api/users/signup', {
       name: e.target[0].value,
@@ -17,11 +21,26 @@ export default function SignUp() {
     }
     else{
       console.log('Signup successful!');
+      setLoadingState(false);
+      navigateTo();
     }
-  }
-    
+  };
+  useEffect((e) => {
+    // temp(e, "cipla");
+
+    setLoadingState(false);
+  }, []);
+  let [loadingState, setLoadingState] = useState(false);
     return (
       <>
+      <LoadingScreen
+        loading={loadingState}
+        bgColor="#f1f1f1"
+        spinnerColor="#00d09c"
+        textColor="#00d09c"
+        // logoSrc="/logo.png"
+        text="Logging You In"
+      />
       <LoginNav/>
       <form onSubmit={handleSubmit}>
         <h3>Sign Up</h3>
